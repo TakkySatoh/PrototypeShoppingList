8e1190fdb5cf93514762eaa399f170633c79ce14
takkysato <takkysatoh@hotmail.com>
2019-02-18 17:15:27 +0900

    フラグメント切り替え見本作成 動作良好 Spinnerのドロップダウン位置調整検討中

--- app/src/main/java/asia/takkyssquare/android/MainActivity.java
+++ app/src/main/java/asia/takkyssquare/android/MainActivity.java
@@ -1,13 +1,57 @@
 package asia.takkyssquare.android;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            replaceFragment(0);
+        }
+
+        Spinner spinner = toolbar.findViewById(R.id.spListName);
+        ArrayAdapter<CharSequence> spAdapter = ArrayAdapter.createFromResource(this, R.array.shopping_list, android.R.layout.simple_list_item_1);
+        spAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(spAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                replaceFragment(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void replaceFragment(int position) {
+        Fragment fragment = new MainFragment();
+        Bundle bundle = new Bundle();
+        String message = getResources().getStringArray(R.array.messages)[position];
+        bundle.putString("message", message);
+        fragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
